3b343bdfa097da3ce7d7a19a41ea46be8421dddf
Arlando Battle <arlando@gmail.com>
2012-09-08 14:42:28 -0400

    README.md
    Added Read me to the repo.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<title>SALE</title>
-	<meta name="description" content="SALE Providence, RI">
+	<meta name="description" content="SALE">
 	<meta name="author" content="Arlando Battle">
 
 	<meta name="viewport" content="width=device-width">
